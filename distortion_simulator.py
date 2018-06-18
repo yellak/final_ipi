d82d21c3e912bae7efd607ede851a5f9e93dbf8d
daniloinacio <daniloinacio@aluno.unb.br>
2018-06-18 08:19:40 -0300

    alterações na função error_diffusion

--- distortion_simulator.py
+++ distortion_simulator.py
@@ -5,18 +5,25 @@ import numpy as np
 # Função que aplica uma difusão de erros usando o método de Floyd-Steinberg.
 def error_diffusion(img, size=(1, 1)):
         img = np.float32(img)
+        #coloca uma borda preta na imagem
+        img_exp = np.zeros((img.shape[0] + 2, img.shape[1] + 2))
+        img_exp[1:img.shape[0] + 1, 1:img.shape[1] + 1] = img[:, :]
 
-        for y in range(0, size[1] - 1):
-                for x in range(1, size[0] - 1):
-                        old_pixel = img[x, y]
-                        img[x, y] = 255 if old_pixel > 127 else 0
-                        quant_error = old_pixel - img[x, y]
-                        img[x + 1, y] = img[x + 1, y] + 7 / 16.0 * quant_error
-                        img[x - 1, y + 1] = img[x - 1, y + 1] + 3 / 16.0 * quant_error
-                        img[x, y + 1] = img[x, y + 1] + 5 / 16.0 * quant_error
-                        img[x + 1, y + 1] = img[x + 1, y + 1] + 1 / 16.0 * quant_error
-        #binariza a imagem
-        ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+        for y in range(1, size[1] + 1):
+                for x in range(1, size[0] + 1):
+                        old_pixel = img_exp[x, y]
+                        if old_pixel > 127:
+                            img_exp[x, y] = 255
+                        else:
+                            img_exp[x, y] = 0
+                        quant_error = old_pixel - img_exp[x, y]
+                        img_exp[x + 1, y] = img_exp[x + 1, y] + 7 / 16.0 * quant_error
+                        img_exp[x - 1, y + 1] = img_exp[x - 1, y + 1] + 3 / 16.0 * quant_error
+                        img_exp[x, y + 1] = img_exp[x, y + 1] + 5 / 16.0 * quant_error
+                        img_exp[x + 1, y + 1] = img_exp[x + 1, y + 1] + 1 / 16.0 * quant_error
+
+        #retira borda
+        img[:, :] = img_exp[1:img.shape[0] + 1, 1:img.shape[1] + 1]
 
         return np.uint8(img)
 
@@ -31,7 +38,7 @@ def print_scan(img, K=1):
         # aplica um error diffusion
         resized_img = error_diffusion(resized_img, resized_img.shape)
         # aplica um filtro de média
-        kernel = np.ones((3, 3), np.float32) / 9
+        kernel = np.ones((K, K), np.float32) / K**2
         resized_img = cv2.filter2D(resized_img, -1, kernel)
         resized_img = np.uint8(resized_img)
         # volta a imagem para o tamanho original
