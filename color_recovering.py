47009548b7630caeea800d7712638b6f869cbd87
Kálley Wilkerson <yellakkalley@hotmail.com>
2018-06-18 22:10:00 -0300

    Arrumando PSNR; estética

--- color_recovering.py
+++ color_recovering.py
@@ -1,42 +1,41 @@
 import cv2
 import numpy as np
 import pywt
-import color_incorporation as inc
+
 
 # Função que faz todo o procedimento inverso
 def color_recover(img_text, size):
+    # Fazendo a transformada Wavelet discreta 2D
+    Coef = pywt.wavedec2(img_text, 'db1', level=2)
+    CA2, (CH2, CV2, CD2), (CH1, CV1, CD1) = Coef
 
-        # Fazendo a transformada Wavelet discreta 2D
-        Coef = pywt.wavedec2(img_text, 'db1', level=2)
-        CA2, (CH2, CV2, CD2), (CH1, CV1, CD1) = Coef
+    # redimensiona CD2
+    RedCD2 = cv2.resize(CD2, dsize=(CV1.shape[1], CV1.shape[0]), interpolation=cv2.INTER_AREA)
 
-        # redimensiona CD2
-        RedCD2 = cv2.resize(CD2, dsize=(CV1.shape[1], CV1.shape[0]), interpolation=cv2.INTER_AREA)
+    # recupear o Cb e o Cr
+    Cb = np.abs(CV1) - np.abs(RedCD2)
+    Cr = np.abs(CH1) - np.abs(CD1)
 
-        # recupear o Cb e o Cr
-        Cb = np.abs(CV1) - np.abs(RedCD2)
-        Cr = np.abs(CH1) - np.abs(CD1)
+    # redimensiona o Cb e o Cr
+    Cb = cv2.resize(Cb, dsize=(size[1], size[0]), interpolation=cv2.INTER_AREA)
+    Cr = cv2.resize(Cr, dsize=(size[1], size[0]), interpolation=cv2.INTER_AREA)
 
-        # redimensiona o Cb e o Cr
-        Cb = cv2.resize(Cb, dsize=(size[1], size[0]), interpolation=cv2.INTER_AREA)
-        Cr = cv2.resize(Cr, dsize=(size[1], size[0]), interpolation=cv2.INTER_AREA)
+    CH1 = np.zeros((CH1.shape[0], CH1.shape[1]))
+    CV1 = np.zeros((CV1.shape[0], CV1.shape[1]))
+    CD2 = np.zeros((CD2.shape[0], CD2.shape[1]))
+    CD1 = np.zeros((CD1.shape[0], CD1.shape[1]))
+    Coef = CA2, (CH2, CV2, CD2), (CH1, CV1, CD1)
 
-        CH1 = np.zeros((CH1.shape[0], CH1.shape[1]))
-        CV1 = np.zeros((CV1.shape[0], CV1.shape[1]))
-        CD2 = np.zeros((CD2.shape[0], CD2.shape[1]))
-        CD1 = np.zeros((CD1.shape[0], CD1.shape[1]))
-        Coef = CA2, (CH2, CV2, CD2), (CH1, CV1, CD1)
+    # recupera o Y a partir da transformada wavelet inversa
+    Y = pywt.waverec2(Coef, 'db1')
 
-        # recupera o Y a partir da transformada wavelet inversa
-        Y = pywt.waverec2(Coef, 'db1')
+    # garante que o Y possua o mesmo tamanho da imagem texturizada
+    Y = cv2.resize(Y, dsize=(size[1], size[0]), interpolation=cv2.INTER_AREA)
 
-        # garante que o Y possua o mesmo tamanho da imagem texturizada
-        Y = cv2.resize(Y, dsize=(size[1], size[0]), interpolation=cv2.INTER_AREA)
+    # faz o merge dos três canais
+    YCrCb = np.zeros((size[0], size[1], 3), np.float32)
+    YCrCb[:, :, 0] = Y
+    YCrCb[:, :, 1] = Cr
+    YCrCb[:, :, 2] = Cb
 
-        # faz o merge dos três canais
-        YCrCb = np.zeros((size[0], size[1], 3), np.float32)
-        YCrCb[:, :, 0] = Y
-        YCrCb[:, :, 1] = Cr
-        YCrCb[:, :, 2] = Cb
-
-        return YCrCb
+    return YCrCb
--- main.py
+++ main.py
@@ -5,21 +5,27 @@ import matplotlib.pyplot as plt
 import cv2
 import sys
 import glob
+import numpy as np
 
 PLOT_GRAPHICS = 1
 NOT_PLOT_PSNR = 2
 
+
+# Função para mostrar o valor PSNR obitido
 def print_PNR(img_origin, img_result):
     psnr = 0
-    img_origin = cv2.cvtColor(img_origin, cv2.COLOR_BGR2YCrCb)
-    img_result = cv2.cvtColor(img_result, cv2.COLOR_BGR2YCrCb)
-    
-    psnr += cv2.PSNR(img_origin[:, :, 0], img_result[:, :, 0])
-   
+
+    Bo, Go, Ro = cv2.split(img_origin)
+    Br, Gr, Rr = cv2.split(img_result)
+    conc_origin = np.concatenate((Bo, Go, Ro))
+    conc_result = np.concatenate((Br, Gr, Rr))
+    psnr += cv2.PSNR(conc_origin, conc_result)
+
     print("O valor Peak-SNR obtido foi:")
     print("%.3f" % psnr)
 
 
+# Função para plotar os resultados obtidos
 def print_(img_origin, img_text, img_result):
 
     plt.subplot(131)
@@ -37,92 +43,88 @@ def print_(img_origin, img_text, img_result):
     plt.show()
 
 
+# Parte principal do programa
 if __name__ == "__main__":
-        printar = 0
+    printar = 0
 
-        if '-p' in sys.argv:
-                printar = 1
+    # Flag para printar os resultados
+    if '-p' in sys.argv:
+        printar = 1
 
-        if '--all' in sys.argv:
-                print("Processando para todas as imagens da pasta ", end="")
-                print("Imagens...")
-                print("Isso pode demorar um pouco")
-                printar = NOT_PLOT_PSNR
-                for fl in glob.glob("Imagens/*.png"):
-                    img = cv2.imread('Imagens/%s' % fl[8:])
-                    img_text = inc.color_incorporation(img)
-                    # Salvando a imagem resultante
-                    cv2.imwrite("Texturizadas/%s" % fl[8:], img_text)
+    # Flag para realizar o processo para todas as imagens
+    if '--all' in sys.argv:
+        print("Processando para todas as imagens da pasta ", end = "")
+        print("Imagens...")
+        printar = NOT_PLOT_PSNR
+        for fl in glob.glob("Imagens/*.png"):
+            print("Imagem %s..." % fl[8:], end = " ")
+            img = cv2.imread('Imagens/%s' % fl[8:])
+            img_text = inc.color_incorporation(img)
 
-                    img_rec = rec.color_recover(img_text, img.shape)
+            # Salvando a imagem resultante
+            cv2.imwrite("Texturizadas/%s" % fl[8:], img_text)
 
-                    cv2.imwrite('Crominâncias/Y%s' % fl[8:], img_rec[:, :, 0])
-                    cv2.imwrite('Crominâncias/Cr%s' % fl[8:], img_rec[:, :, 1])
-                    cv2.imwrite('Crominâncias/Cb%s' % fl[8:], img_rec[:, :, 2])
-    
-                    # converte a imagem recuperada para BGR
-                    img_rec = inc.Convert_YCC2BGR(img_rec)
-                    cv2.imwrite('Resultados/%s' % fl[8:], img_rec)
-            
-                    img_rec = cv2.imread('Resultados/%s' % fl[8:])
-                    if printar == 1:
-                        print_(img, img_text, img_rec)
-                        print_PNR(img, img_rec)
-       
-        else:
-                print("Digite o nome da imagem que você deseja transformar:")
-                img_name = input()
-                img = cv2.imread('Imagens/%s' % img_name)
-                if img is None:
-                    print("Imagem %s não encontrada na pasta Imagens" % img_name)
-                    exit(1)
+            img_rec = rec.color_recover(img_text, img.shape)
 
-                img_text = inc.color_incorporation(img)
-                # Salvando a imagem resultante
-                cv2.imwrite("Texturizadas/%s" % img_name, img_text)
-               
-                # simulação
-                simulation = -1
-                while(simulation < 0 or simulation > 1):
-                        print("\nSimular impressão?")
-                        print("0. Não")
-                        print("1. Sim")
-                        simulation = int(input())
+            # converte a imagem recuperada para BGR
+            img_rec = inc.Convert_YCC2BGR(img_rec)
+            cv2.imwrite('Resultados/%s' % fl[8:], img_rec)
 
-                if(simulation == 1):
-                    k = int(input("Qual a ordem do resize?\n"))
-                    print("Simulando distorção por impressão...")
+            img_rec = cv2.imread('Resultados/%s' % fl[8:])
+            if printar == 1:
+                print_(img, img_text, img_rec)
+                print_PNR(img, img_rec)
+            print("Feito")
 
-                    img_text = cv2.imread('Texturizadas/%s' % img_name, 0)
-                    if img_text is None:
-                        print("Imgagem %s não encontrada na pasta Texturizadas" % img_name)
-                        exit(1)
+    else:
+        print("Digite o nome da imagem que você deseja transformar:")
+        img_name = input()
+        img = cv2.imread('Imagens/%s' % img_name)
+        if img is None:
+            print("Imagem %s não encontrada na pasta Imagens" % img_name)
+            exit(1)
 
-                    img_text = sdist.print_scan(img_text, k)
-                    cv2.imwrite('Texturizadas/%s' % img_name, img_text)
+        # Função que irá incorporar as cores na imagem cinza
+        img_text = inc.color_incorporation(img)
+        # Salvando a imagem resultante
+        cv2.imwrite("Texturizadas/%s" % img_name, img_text)
 
-                img_text = cv2.imread('Texturizadas/%s' % img_name, 0)
-                if img_text is None:
-                    print("Imagem %s não encontrada na pasta Texturizadas" % img_name)
-                    exit(1)
+        # Simulação
+        simulation = -1
+        while(simulation < 0 or simulation > 1):
+            print("\nSimular impressão?")
+            print("0. Não")
+            print("1. Sim")
+            simulation = int(input())
 
-                img_rec = rec.color_recover(img_text, img.shape)
+        if(simulation == 1):
+            k = int(input("Qual a ordem do resize?\n"))
+            print("Simulando distorção por impressão...")
 
-                cv2.imwrite('Crominâncias/Y%s' % img_name, img_rec[:, :, 0])
-                cv2.imwrite('Crominâncias/Cr%s' % img_name, img_rec[:, :, 1])
-                cv2.imwrite('Crominâncias/Cb%s' % img_name, img_rec[:, :, 2])
+            img_text = cv2.imread('Texturizadas/%s' % img_name, 0)
+            if img_text is None:
+                print("Imgagem %s não encontrada na pasta Texturizadas" % img_name)
+                exit(1)
 
-                # converte a imagem recuperada para BGR
-                img_rec = inc.Convert_YCC2BGR(img_rec)
-                # img_rec = cv2.cvtColor(img_rec, cv2.COLOR_YCrCb2BGR)
+            # Função que simulará a impressão de um documento
+            img_text = sdist.print_scan(img_text, k)
+            cv2.imwrite('Texturizadas/%s' % img_name, img_text)
 
-                cv2.imwrite('Resultados/%s' % img_name, img_rec)
-                img_rec = cv2.imread('Resultados/%s' % img_name)
-                
-                if printar == 1:
-                    print_(img, img_text, img_rec)
-                    print_PNR(img, img_rec)
-                      
-                # else:
-                #        print("\nPor favor, verifique se digitou o nome da")
-                #        print("imagem corretamente e se ela está na pasta Imagens")
+        img_text = cv2.imread('Texturizadas/%s' % img_name, 0)
+        if img_text is None:
+            print("Imagem %s não encontrada na pasta Texturizadas" % img_name)
+            exit(1)
+
+        # Recuperando as cores da imagem
+        img_rec = rec.color_recover(img_text, img.shape)
+
+        # Converte a imagem recuperada para BGR
+        img_rec = inc.Convert_YCC2BGR(img_rec)
+
+        cv2.imwrite('Resultados/%s' % img_name, img_rec)
+        img_rec = cv2.imread('Resultados/%s' % img_name)
+
+        # Printando os resultados
+        if printar == 1:
+            print_(img, img_text, img_rec)
+        print_PNR(img, img_rec)
